cd927914476d3b6293a000384a9be11ff5c90ff0
Nepomuk Seiler <nepomuk.seiler@mukis.de>
2013-01-07 22:00:56 +0100

    Example for injecting custom objects. -> annotate classe with @Creatable if they should be injected

--- de.lmu.ifi.dbs.medmon.life.e4/src/main/java/de/lmu/ifi/dbs/medmon/life/e4/Start.java
+++ de.lmu.ifi.dbs.medmon.life.e4/src/main/java/de/lmu/ifi/dbs/medmon/life/e4/Start.java
@@ -2,21 +2,22 @@ package de.lmu.ifi.dbs.medmon.life.e4;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Inject;
 
 import org.eclipse.e4.ui.di.Focus;
 import org.eclipse.jface.wizard.WizardDialog;
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
 
 public class Start {
 
-	public Start() {
-	}
+	@Inject
+	private ExportWizard exportWizard;
 
 	/**
 	 * Create contents of the view part.
@@ -25,18 +26,18 @@ public class Start {
 	public void createControls(final Composite parent) {
 		GridLayout gl_parent = new GridLayout(2, false);
 		parent.setLayout(gl_parent);
-		
+
 		Button btnExport = new Button(parent, SWT.NONE);
 		btnExport.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
-				WizardDialog dialog = new WizardDialog(parent.getShell(), new ExportWizard());
+				WizardDialog dialog = new WizardDialog(parent.getShell(), exportWizard);
 				dialog.open();
 			}
 		});
 		btnExport.setText("Export");
 		new Label(parent, SWT.NONE);
-		
+
 	}
 
 	@PreDestroy
@@ -45,7 +46,6 @@ public class Start {
 
 	@Focus
 	public void setFocus() {
-		// TODO	Set the focus to control
 	}
 
 }
