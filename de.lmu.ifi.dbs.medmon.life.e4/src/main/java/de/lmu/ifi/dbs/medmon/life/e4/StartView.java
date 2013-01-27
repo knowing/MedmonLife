10f7498880f30787334ab83cb886ac002f1bbf16
Nepomuk Seiler <nepomuk.seiler@mukis.de>
2013-01-27 14:19:53 +0100

    Fix preferences error

--- de.lmu.ifi.dbs.medmon.life.e4/src/main/java/de/lmu/ifi/dbs/medmon/life/e4/StartView.java
+++ de.lmu.ifi.dbs.medmon.life.e4/src/main/java/de/lmu/ifi/dbs/medmon/life/e4/StartView.java
@@ -1,28 +1,43 @@
 package de.lmu.ifi.dbs.medmon.life.e4;
 
+import static de.lmu.ifi.dbs.medmon.medic.core.preferences.IMedicPreferences.DATABASE_DIR;
+import static de.lmu.ifi.dbs.medmon.medic.core.preferences.IMedicPreferences.DATABASE_NODE;
+import static de.lmu.ifi.dbs.medmon.medic.core.preferences.IMedicPreferences.MEDMON_DIR;
+import static de.lmu.ifi.dbs.medmon.medic.core.preferences.IMedicPreferences.MEDMON_DPU;
+import static de.lmu.ifi.dbs.medmon.medic.core.preferences.IMedicPreferences.MEDMON_NODE;
+import static de.lmu.ifi.dbs.medmon.medic.core.preferences.IMedicPreferences.MEDMON_PATIENT;
+import static de.lmu.ifi.dbs.medmon.medic.core.preferences.IMedicPreferences.MEDMON_TMP;
+
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.inject.Inject;
 
+import org.eclipse.core.runtime.preferences.IEclipsePreferences;
+import org.eclipse.e4.core.di.extensions.Preference;
 import org.eclipse.e4.ui.di.Focus;
 import org.eclipse.e4.ui.services.IStylingEngine;
 import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.CLabel;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Link;
+import org.eclipse.wb.swt.SWTResourceManager;
+import org.osgi.service.prefs.BackingStoreException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import de.lmu.ifi.dbs.medmon.life.e4.wizards.DeletionWizard;
 import de.lmu.ifi.dbs.medmon.life.e4.wizards.ExportWizard;
 import de.lmu.ifi.dbs.medmon.life.e4.wizards.ImportWizard;
-import org.eclipse.swt.custom.CLabel;
-import org.eclipse.wb.swt.SWTResourceManager;
-import org.eclipse.swt.layout.GridData;
 
 public class StartView {
+
+	private static final Logger log = LoggerFactory.getLogger(StartView.class);
 
 	@Inject
 	private ExportWizard exportWizard;
@@ -30,27 +45,26 @@ public class StartView {
 	private ImportWizard importWizard;
 	@Inject
 	private DeletionWizard deletionWizard;
-	
-	@Inject IStylingEngine styleEngine;
+
+	@Inject
+	IStylingEngine styleEngine;
 
 	/**
 	 * Create contents of the view part.
 	 */
 	@PostConstruct
 	public void createControls(final Composite parent) {
-		
-		
-		
+
 		styleEngine.setId(parent, "WizardStyle");
 		parent.setBackgroundMode(SWT.INHERIT_DEFAULT);
-		
+
 		GridLayout gl_parent = new GridLayout(4, false);
 		gl_parent.horizontalSpacing = 0;
 		gl_parent.marginWidth = 0;
 		gl_parent.marginHeight = 0;
 		gl_parent.verticalSpacing = 0;
 		parent.setLayout(gl_parent);
-		
+
 		Composite compositeLogo = new Composite(parent, SWT.NONE);
 		GridLayout gl_composite = new GridLayout(1, false);
 		gl_composite.verticalSpacing = 0;
@@ -59,18 +73,17 @@ public class StartView {
 		gl_composite.marginHeight = 0;
 		compositeLogo.setLayout(gl_composite);
 		compositeLogo.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 4, 1));
-		
+
 		CLabel lblLogo = new CLabel(compositeLogo, SWT.NONE);
 		lblLogo.setLayoutData(new GridData(SWT.RIGHT, SWT.TOP, true, false, 1, 1));
 		lblLogo.setImage(SWTResourceManager.getImage(StartView.class, "/icons/life_icon_64.png"));
 		styleEngine.setClassname(compositeLogo, "logo");
 
-
 		Label lblSeparator1 = new Label(parent, SWT.SEPARATOR | SWT.HORIZONTAL);
 		lblSeparator1.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 4, 1));
 		CLabel lblImport = new CLabel(parent, SWT.NONE);
 		lblImport.setImage(SWTResourceManager.getImage(StartView.class, "/icons/import.gif"));
-		
+
 		Link linkImport = new Link(parent, SWT.NONE);
 		linkImport.setText("<a>Import</a>");
 		linkImport.addSelectionListener(new SelectionAdapter() {
@@ -82,28 +95,23 @@ public class StartView {
 		});
 		new Label(parent, SWT.NONE);
 		new Label(parent, SWT.NONE);
-				
+
 		Label lblSeparator2 = new Label(parent, SWT.SEPARATOR | SWT.HORIZONTAL);
 		lblSeparator2.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 4, 1));
 		new Label(parent, SWT.NONE);
 		new Label(parent, SWT.NONE);
-		
+
 		Label lblImportOptions = new Label(parent, SWT.NONE);
 		lblImportOptions.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
 		lblImportOptions.setText("Patient ausw\u00E4hlen/anlegen\r\nZeitraum einschr\u00E4nken");
 		styleEngine.setClassname(lblImportOptions, "startPageText");
 		new Label(parent, SWT.NONE);
 		new Label(parent, SWT.NONE);
-		
+
 		Label lblFiller = new Label(parent, SWT.NONE);
 		lblFiller.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, true, 1, 1));
 		new Label(parent, SWT.NONE);
 		new Label(parent, SWT.NONE);
-
-
-
-		
-
 
 		CLabel lblExport = new CLabel(parent, SWT.NONE);
 		lblExport.setImage(SWTResourceManager.getImage(StartView.class, "/icons/export.gif"));
@@ -120,31 +128,28 @@ public class StartView {
 		new Label(parent, SWT.NONE);
 		new Label(parent, SWT.NONE);
 
-		
 		Label label = new Label(parent, SWT.SEPARATOR | SWT.HORIZONTAL);
 		label.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 4, 1));
-				new Label(parent, SWT.NONE);
-				new Label(parent, SWT.NONE);
-				
-										
+		new Label(parent, SWT.NONE);
+		new Label(parent, SWT.NONE);
+
 		Label lblExportOptions = new Label(parent, SWT.NONE);
 		lblExportOptions.setText("Filteroptionen\r\nExportoptionen");
 		styleEngine.setClassname(lblExportOptions, "startPageText");
 		new Label(parent, SWT.NONE);
 		new Label(parent, SWT.NONE);
-		
+
 		Label lblFiller2 = new Label(parent, SWT.NONE);
 		lblFiller2.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, true, 1, 1));
 		new Label(parent, SWT.NONE);
 		new Label(parent, SWT.NONE);
-	
-		
+
 		CLabel lblDelete = new CLabel(parent, SWT.NONE);
 		lblDelete.setImage(SWTResourceManager.getImage(StartView.class, "/icons/delete.gif"));
 
 		Link linkDelete = new Link(parent, SWT.NONE);
 		linkDelete.setText("<a>Patienten/Daten l\u00F6schen</a>");
-		
+
 		linkDelete.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
@@ -155,17 +160,33 @@ public class StartView {
 		new Label(parent, SWT.NONE);
 		new Label(parent, SWT.NONE);
 		new Label(parent, SWT.NONE);
-		
+
 		Label lblFiller3 = new Label(parent, SWT.NONE);
 		lblFiller3.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, true, 1, 1));
 		new Label(parent, SWT.NONE);
 		new Label(parent, SWT.NONE);
 
-
 	}
 
 	@PreDestroy
 	public void dispose() {
+	}
+
+	@Inject
+	protected void initPreferences(@Preference(nodePath = MEDMON_NODE) IEclipsePreferences medNode,
+			@Preference(nodePath = DATABASE_NODE) IEclipsePreferences dbNode) throws BackingStoreException {
+		log.info("Setting preferences");
+		String userHome = System.getProperty("user.home");
+		String sep = System.getProperty("file.separator");
+		String medmonHome = userHome + sep + ".medmon";
+		medNode.put(MEDMON_DIR, medmonHome);
+		medNode.put(MEDMON_DPU, medmonHome + sep + "dpu");
+		medNode.put(MEDMON_PATIENT, medmonHome + sep + "patients");
+		medNode.put(MEDMON_TMP, medmonHome + sep + ".tmp");
+		medNode.flush();
+
+		dbNode.put(DATABASE_DIR, medmonHome + sep + "db");
+		dbNode.flush();
 	}
 
 	@Focus
