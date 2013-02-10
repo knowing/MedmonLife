2047b34fdada90600aef16a68f21339215987ee1
Nepomuk Seiler <nepomuk.seiler@mukis.de>
2013-02-10 15:23:33 +0100

    Use TypedQuery for typesafety

--- de.lmu.ifi.dbs.medmon.life.e4/src/main/java/de/lmu/ifi/dbs/medmon/life/e4/provider/PatientContentProvider.java
+++ de.lmu.ifi.dbs.medmon.life.e4/src/main/java/de/lmu/ifi/dbs/medmon/life/e4/provider/PatientContentProvider.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
-import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 
 import org.eclipse.e4.core.di.annotations.Creatable;
 import org.eclipse.gemini.ext.di.GeminiPersistenceContext;
@@ -15,30 +15,30 @@ import de.lmu.ifi.dbs.medmon.database.entity.Patient;
 
 @Creatable
 public class PatientContentProvider implements IStructuredContentProvider {
-	
+
 	@Inject
 	@GeminiPersistenceContext(unitName = "medmon")
 	EntityManager em;
-	
+
 	@Override
 	public void dispose() {
 	}
-	
+
 	@Override
 	public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
 	}
-	
+
 	@Override
 	public Object[] getElements(Object inputElement) {
-		if(inputElement instanceof Patient[])
-			return (Patient[])inputElement;
+		if (inputElement instanceof Patient[])
+			return (Patient[]) inputElement;
 		return getPatients();
 	}
-	
+
 	private Patient[] getPatients() {
-		Query allPatients = em.createNamedQuery("Patient.findAll");
+		TypedQuery<Patient> allPatients = em.createNamedQuery("Patient.findAll", Patient.class);
 		List<Patient> patients = allPatients.getResultList();
-		if(patients.isEmpty())
+		if (patients.isEmpty())
 			return new Patient[0];
 		return patients.toArray(new Patient[patients.size()]);
 	}
